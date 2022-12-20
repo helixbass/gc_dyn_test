f33a7d8172a0a8648b6b10d191e04b53e2add099
Julian Rosse <julian@helixbass.net>
2022-12-20 13:35:04 -0500

    transmute

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
-use gc::{Finalize, Gc, Trace};
+use gc::{Finalize, Gc, GcCell, Trace};
+use std::mem;
 
 trait Trait: Trace + Finalize {
     fn print(&self);
 }
 
 #[derive(Trace, Finalize)]
-struct Foo;
+struct Foo {
+    a: String,
+    _dyn_wrapper: GcCell<Option<Gc<Box<dyn Trait>>>>,
+}
+
+impl Foo {
+    pub fn new() -> Gc<Box<Self>> {
+        let dyn_wrapper: Gc<Box<dyn Trait>> = Gc::new(Box::new(Self {
+            a: "Hello".to_owned(),
+            _dyn_wrapper: Default::default(),
+        }));
+        let downcasted: Gc<Box<Foo>> = unsafe { mem::transmute(dyn_wrapper.clone()) };
+        *downcasted._dyn_wrapper.borrow_mut() = Some(dyn_wrapper);
+        downcasted
+    }
+
+    pub fn as_dyn_trait(&self) -> Gc<Box<dyn Trait>> {
+        self._dyn_wrapper.borrow().clone().unwrap()
+    }
+
+    pub fn print_from_foo(&self) {
+        println!("{}", self.a);
+    }
+}
 
 impl Trait for Foo {
     fn print(&self) {
@@ -14,6 +38,7 @@ impl Trait for Foo {
 }
 
 fn main() {
-    let dyn_trait: Gc<Box<dyn Trait>> = Gc::new(Box::new(Foo));
-    dyn_trait.print();
+    let foo = Foo::new();
+    foo.as_dyn_trait().print();
+    foo.print_from_foo();
 }
